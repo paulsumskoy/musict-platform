24d2d9158f26060244553b52b08c328c5805445f
paulsumskoy <p.sumskoy@gmail.com>
2021-10-22 16:26:13 +0300

    Server created

---
+++ server/src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from "@nestjs/core"
+import { AppModule } from "./app.modules"
+
+
+const start = async () => {
+    try {
+        const PORT = process.env.PORT || 5000;
+        const app = await NestFactory.create(AppModule);
+
+        await app.listen(PORT, () => console.log('server started on PORT ${PORT}'))
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start()
