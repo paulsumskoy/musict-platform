102a2cd5521ca6e801d898ba4750c42278742089
paulsumskoy <p.sumskoy@gmail.com>
2021-10-31 11:17:53 +0200

    Nextjs inntegrated

---
+++ client/pages/index.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@material-ui/core";
+import React from "react";
+
+const Index = () => {
+    return (
+        <div>
+            Main page
+            <Button>Button</Button>
+        </div>
+    );
+};
+
+export default Index;
