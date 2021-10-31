67abec8f46e979d3f5a193ca67582cb396af5688
paulsumskoy <p.sumskoy@gmail.com>
2021-10-31 12:08:18 +0200

    Test style

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,12 +1,27 @@
-import { Button } from "@material-ui/core";
 import React from "react";
+import Navbar from "../components/Navbar";
 
 const Index = () => {
     return (
-        <div>
-            Main page
-            <Button>Button</Button>
+        <>
+        <Navbar/>
+        <div className="center">
+            <h1>Welcome!</h1>
+            <h3>Best tracks!</h3>
         </div>
+
+        <style jsx>
+            {`
+                .center {
+                    margin-top: 150px;
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+                    justify-content: center;
+                }
+            `}
+        </style> 
+        </>
     );
 };
 
