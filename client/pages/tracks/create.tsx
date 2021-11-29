2ad5f06e70cad8a743dabeacd651aeabe585c2d8
paulsumskoy <p.sumskoy@gmail.com>
2021-11-29 19:54:13 +0200

    Upload tsx

---
+++ client/pages/tracks/create.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MainLayout from "../../layouts/MainLayot";
+
+const Create = () => {
+    return (
+        <MainLayout>
+            <h1>Treck Upload</h1>
+        </MainLayout>
+    );
+};
+
+export default Create;
--- client/pages/tracks/index.tsx
+++ client/pages/tracks/index.tsx
@@ -1,10 +1,18 @@
+import { Button, Card, Grid } from "@material-ui/core";
 import React from "react";
 import MainLayout from "../../layouts/MainLayot";
 
 const Index = () => {
     return (
         <MainLayout>
-            Track List
+            <Grid container justifyContent="center">
+                <Card style={{width: 900}}> 
+                    <Grid container justifyContent="space-between">
+                        <h1> Track List</h1>
+                        <Button>Upload</Button>
+                    </Grid>
+                </Card>
+            </Grid>
         </MainLayout>
     );
 };
