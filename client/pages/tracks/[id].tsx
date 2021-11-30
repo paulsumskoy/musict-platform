bfdc748621c943ae4e42d613406707101b1cd8c6
paulsumskoy <p.sumskoy@gmail.com>
2021-11-30 14:12:29 +0200

    Improvements

--- client/pages/tracks/[id].tsx
+++ client/pages/tracks/[id].tsx
@@ -1,18 +1,54 @@
-import { Button } from "@material-ui/core";
+import { Button, Grid, TextField } from "@material-ui/core";
 import { useRouter } from "next/router";
 import React from "react";
 import MainLayout from "../../layouts/MainLayot";
 import { ITrack } from "../../types/tracks";
 
 const TrackPage = () => {
-    const track: ITrack = {_id: '1', name: "Walking", artist: "Jesper Kyd", text: "lalala", listens: 5, audio: 'http://localhost:5000/audio/first.mp3', picture: "http://localhost:5000/image/first.jpg", comments: []},
+    const track: ITrack = {_id: '1', name: "Walking", artist: "Jesper Kyd", text: "lalala", listens: 5, audio: 'http://localhost:5000/audio/first.mp3', picture: "http://localhost:5000/image/first.jpg", comments: []}
     const router = useRouter()
 
     return(
         <MainLayout>
-            <Button onClick={router.push('/tracks')}>
+            <Button 
+                variant={"outlined"}
+                style={{fontSize: 32}}
+                onClick={() =>router.push('/tracks')}
+            >
                 To the list
             </Button>
+            <Grid container style={{margin: '20px 0'}}>
+                <img src={track.picture} width={200} height={200}/>
+                <div style={{marginLeft: '30'}}>
+                    <h1>Track name - {track.name}</h1>
+                    <h1>Artist - {track.artist}</h1>
+                    <h1>Listens - {track.listens}</h1>
+                </div>
+            </Grid>
+            <h1>Lyrics</h1>
+            <p>{track.text}</p>
+            <h1>Comments</h1>
+            <Grid container>
+                <TextField
+                    label="Your name"
+                    fullWidth
+                />
+                <TextField
+                    label="Comment"
+                    fullWidth
+                    multiline
+                    rows={4}
+                />
+                <Button>Send</Button>
+            </Grid>
+            <div>
+                {track.comments.map(comment =>
+                  <div>
+                      <div>User Name - {comment.username}</div>
+                      <div>Comment - {comment.text}</div>
+                  </div>  
+                )}
+            </div>
         </MainLayout>
     );
 };
