c05ee9d44b465d81f656897fa50d2f12e59faabb
paulsumskoy <p.sumskoy@gmail.com>
2021-11-30 17:43:00 +0200

    Improvements

--- client/components/FileUpload.tsx
+++ client/components/FileUpload.tsx
@@ -1,16 +1,15 @@
 import React, { useRef } from "react";
 
 interface FileUploadProps {
-    file: any;
     setFile: Function
     accept: string;
 }
 
-const FileUpload: React.FC<FileUploadProps> = ({file, setFile, accept, children}) => {
+const FileUpload: React.FC<FileUploadProps> = ({setFile, accept, children}) => {
     const ref = useRef<HTMLInputElement>()
 
     const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        console.log(e.target.files)
+        setFile(e.target.files[0])
     }
 
     return(
--- client/pages/tracks/create.tsx
+++ client/pages/tracks/create.tsx
@@ -5,7 +5,10 @@ import StepWrapper from "../../components/StepWrapper";
 import MainLayout from "../../layouts/MainLayot";
 
 const Create = () => {
-    const[activeStep, setActiveStep] = useState(0)
+    const [activeStep, setActiveStep] = useState(0)
+    const [picture, setPicture] = useState(null)
+    const [audio, setAudio] = useState(null)
+
     const next = () => {
         if (activeStep !== 2) {
             setActiveStep(prev => prev + 1)
@@ -39,12 +42,14 @@ const Create = () => {
                 </Grid>
                 }
                 {activeStep === 1 &&
-                    <FileUpload file={''} setFile={() =>({})} accept="/image*">
+                    <FileUpload setFile={setPicture} accept="image/*">
                         <Button>Upload the picture</Button>
                     </FileUpload>
                 }
                 {activeStep === 2 &&
-                    <h1>Step 3</h1>
+                    <FileUpload setFile={setAudio} accept="audio/*">
+                        <Button>Upload the audio</Button>
+                    </FileUpload>
                 }
             </StepWrapper>
             <Grid container justifyContent='space-between'>
