75eeeb244b9429882065cdc1860c119d260ac3c6
paulsumskoy <p.sumskoy@gmail.com>
2021-11-30 09:10:17 +0200

    Minor changes

--- client/pages/tracks/index.tsx
+++ client/pages/tracks/index.tsx
@@ -1,16 +1,22 @@
-import { Button, Card, Grid } from "@material-ui/core";
+import { Box, Button, Card, Grid } from "@material-ui/core";
 import React from "react";
 import MainLayout from "../../layouts/MainLayot";
+import { useRouter } from "next/router";
 
 const Index = () => {
+    const router = useRouter()
     return (
         <MainLayout>
             <Grid container justifyContent="center">
                 <Card style={{width: 900}}> 
+                <Box p={3}>
                     <Grid container justifyContent="space-between">
                         <h1> Track List</h1>
-                        <Button>Upload</Button>
+                        <Button onClick={() => router.push('/tracks/create')}>
+                            Upload
+                        </Button>
                     </Grid>
+                </Box>
                 </Card>
             </Grid>
         </MainLayout>
