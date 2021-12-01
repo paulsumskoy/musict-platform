246eb37eb024e3db1c284ba2b6545a1c11550a96
paulsumskoy <p.sumskoy@gmail.com>
2021-12-01 10:18:07 +0200

    Improvements to playerReducer

--- client/store/reducers/playerReducer.ts
+++ client/store/reducers/playerReducer.ts
@@ -1,7 +1,28 @@
-const initialization = {
+import { PlayerAction, PlayerActionTypes, PlayerState } from "../../types/player"
 
+const initialState: PlayerState = {
+    currenTime: 0,
+    duration: 0,
+    active: null,
+    volume: 0,
+    pause: true
 }
 
-export const playerReducer = (state = initialization, action) => {
-
+export const playerReducer = (state = initialState, action: PlayerAction): PlayerState => {
+    switch (action.type) {
+        case PlayerActionTypes.PAUSE:   
+            return {...state, pause: true}
+        case PlayerActionTypes.PLAY:   
+            return {...state, pause: false}
+        case PlayerActionTypes.SET_CURRENT_TIME:   
+            return {...state, currenTime: action.payload}
+        case PlayerActionTypes.SET_VOLUME:   
+            return {...state, volume: action.payload}
+        case PlayerActionTypes.SET_DURATION:   
+            return {...state, duration: action.playload}
+        case PlayerActionTypes.SET_ACTIVE:   
+            return {...state, active: action.playload, duration: 0, currenTime: 0}    
+        default:
+            return state
+    }
 }
