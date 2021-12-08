f9b518cd771dcc8a8ec3ccfc973a01f004b8603a
paulsumskoy <p.sumskoy@gmail.com>
2021-12-08 09:26:59 +0200

    Add comment implemented

--- client/pages/tracks/[id].tsx
+++ client/pages/tracks/[id].tsx
@@ -1,25 +1,44 @@
 import { Button, Grid, TextField } from "@material-ui/core";
+import { PanoramaSharp } from "@material-ui/icons";
+import axios from "axios";
+import { GetServerSideProps } from "next";
 import { useRouter } from "next/router";
 import React, { useState } from "react";
+import { useInput } from "../../hooks/useInput";
 import MainLayout from "../../layouts/MainLayout";
 import { ITrack } from "../../types/track";
 
 const TrackPage = ({serverTrack}) => {
     const [track, setTrack] = useState<ITrack>(serverTrack)
     const router = useRouter()
+    const username = useInput('')
+    const text = useInput('')
+
+    const addComment = async () => {
+        try {
+            const response = await axios.post('http://localhost:5000/tracks/comment', {
+                username: username.value,
+                text: text.value,
+                trackId: track._id
+            })
+            setTrack({...track, comments: [...track.comments, response.data]})
+        } catch (e) {
+            console.log(e)
+        }
+    }
 
     return(
         <MainLayout>
             <Button 
                 variant={"outlined"}
                 style={{fontSize: 32}}
-                onClick={() =>router.push('/tracks')}
+                onClick={() => router.push('/tracks')}
             >
                 To the list
             </Button>
             <Grid container style={{margin: '20px 0'}}>
                 <img src={'http://localhost:5000/' + track.picture} width={200} height={200}/>
-                <div style={{marginLeft: '30'}}>
+                <div style={{marginLeft: 30}}>
                     <h1>Track name - {track.name}</h1>
                     <h1>Artist - {track.artist}</h1>
                     <h1>Listens - {track.listens}</h1>
@@ -32,14 +51,16 @@ const TrackPage = ({serverTrack}) => {
                 <TextField
                     label="Your name"
                     fullWidth
+                    {...username}
                 />
                 <TextField
                     label="Comment"
+                    {...text}
                     fullWidth
                     multiline
                     rows={4}
                 />
-                <Button>Send</Button>
+                <Button onClick={addComment}>Send</Button>
             </Grid>
             <div>
                 {track.comments.map(comment =>
@@ -54,3 +75,12 @@ const TrackPage = ({serverTrack}) => {
 };
 
 export default TrackPage;
+
+export const getServerSideProps: GetServerSideProps = async ({params}) => {
+    const response = await axios.get('http://localhost:5000/tracks/' + params.id)
+    return {
+        props: {
+            serverTrack: response.data
+        }
+    }
+}
--- client/pages/tracks/create.tsx
+++ client/pages/tracks/create.tsx
@@ -1,17 +1,34 @@
 import { Button, Grid, TextField } from "@material-ui/core";
+import axios from "axios";
+import router, { useRouter } from "next/router";
 import React, { useState } from "react";
 import FileUpload from "../../components/FileUpload";
 import StepWrapper from "../../components/StepWrapper";
+import { useInput } from "../../hooks/useInput";
 import MainLayout from "../../layouts/MainLayout";
 
 const Create = () => {
     const [activeStep, setActiveStep] = useState(0)
     const [picture, setPicture] = useState(null)
     const [audio, setAudio] = useState(null)
+    const name = useInput('')
+    const artist = useInput('')
+    const text = useInput('')
+    const router = useRouter()
 
     const next = () => {
         if (activeStep !== 2) {
             setActiveStep(prev => prev + 1)
+        } else {
+            const formData = new FormData()
+            formData.append('name', name.value)
+            formData.append('text', text.value)
+            formData.append('artist', artist.value)
+            formData.append('picture', picture)
+            formData.append('audio', audio)
+            axios.post('Http://localhost:5000/tracks', formData)
+                .then(resp => router.push('/tracks'))
+                .catch(e => console.log(e))
         }
     }
 
@@ -25,15 +42,18 @@ const Create = () => {
                 {activeStep === 0 &&
                 <Grid container direction={"column"} style={{padding: 20}}>
                     <TextField
+                        {...name}
                         style={{marginTop: 10}}
                         label={"Name of the track"}
                     />
                     <TextField
+                        {...artist}
                         style={{marginTop: 10}}
                         label={"Artist name"}
                         
                     />
                     <TextField
+                        {...text}
                         style={{marginTop: 10}}
                         label={"Lyrics"}
                         multiline
